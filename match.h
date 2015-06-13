065dc54b0bb9fe9c0066013fe3a566765d97821d
DamirHamzic <DamirHamzic@users.noreply.github.com>
2015-06-13 14:24:11 +0200

    Update match.h

--- match.h
+++ match.h
@@ -1,38 +1,43 @@
 #ifndef MATCH_H
 #define MATCH_H
 
+#include "Player.h"
 
 class Match
 {
     public:
-        Match();
-        player Get_name_of_first_player() { return _name_of_first_player; }
-        void Set_name_of_first_player(player val) { _name_of_first_player = val; }
-        player Get_name_of_second_player() { return _name_of_second_player; }
-        void Set_name_of_second_player(player val) { _name_of_second_player = val; }
-        string Get_result() { return _result; }
-        void Set_result(string val) { _result = val; }
-        void Set_Match(player name_first,player name_second,string result);
+        Match()=default;
+		Match(const Match& match);
+		
+        const player& Get_first_player() const;
+        void Set_first_player(player val);
+        const player& Get_second_player() const;
+        void Set_second_player(player val);
+        const std::string& Get_result() const;
+        void Set_result(std::string val);
+        void Set_Match(player name_first,player name_second,std::string result);
 
-        friend ostream& operator<<(ostream& out,const Match& val )
+        friend std::ostream& operator<<(std::ostream& out,const Match& val )
         {
-            out<<val._name_of_first_player<<" - "<<val._name_of_second_player<<"Result: "<<val._result;
+            out<<val._first_player<<" - "<<val._second_player<<"Result: "<<val._result;
+            return out;
 
         }
         /*
         friend istream& operator<<(istream& in,player& val )
         {
 
-             in>>val._name_of_first_player;
-             in>>val._name_of_second_player;
+             in>>val._first_player;
+             in>>val._second_player;
              in>>val._result;
+             return in;
         }
         */
     protected:
     private:
-        player _name_of_first_player;
-        player _name_of_second_player;
-        string _result;
+        player _first_player;
+        player _second_player;
+        std::string _result;
 };
 
 #endif // MATCH_H
