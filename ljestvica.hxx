d3967bfdb51663a2168edf908e1f5526b48a4f6a
Almir <almirmulke4@gmail.com>
2015-06-13 18:11:05 +0200

    Dodana klasa ATP

---
+++ ljestvica.hxx
@@ -0,0 +1,17 @@
+#ifndef _LJESTVICA_H
+#define _LJESTVICA_H
+#include "list.hxx"
+#include "Player.h"
+#include "match.h"
+
+class ATP{
+private: 
+list<player> players;
+//Grand_slams<grand_slam>;
+//Master_1000<master1000>;
+//Master_500<master500>;
+//Master_250<master250>;
+}; 
+
+
+#endif
